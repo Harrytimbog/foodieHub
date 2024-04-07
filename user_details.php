e99d9499b6a062a7cadbb0793ace6143ec0e14dc
Timilehin Arigbede <harriemannie@gmail.com>
2024-04-07 22:23:34 +0100

    Add Favicon to App

--- user_details.php
+++ user_details.php
@@ -39,7 +39,10 @@ try {
     <link rel="stylesheet" href="/css/recipes.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/footer.css">
-    
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+    <!-- <link rel="manifest" href="/site.webmanifest"> -->
   </head>
     <body>
       <!-- NAVBAR -->
